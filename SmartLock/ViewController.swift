4e06999cf87667490c22a2eeb4aa4ffe80243336
Ben Faria <BenFaria@Users-MacBook-Pro.local>
2017-10-08 02:31:13 -0400

    yes!

--- SmartLock/ViewController.swift
+++ SmartLock/ViewController.swift
@@ -8,28 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     var simpleBluetoothIO: SimpleBluetoothIO!
     
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var numberField: UITextField!
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         simpleBluetoothIO = SimpleBluetoothIO(serviceUUID: "a3c9025d-f942-48fb-bb7a-c2397c029f85", delegate: self)
     }
     
-    @IBAction func sendString(_ sender: Any) {
-        simpleBluetoothIO.writeValue(value: 8)
+    func generateString(name: String, number: String) -> String
+    {
+        var newName = ""
+        for c in name
+        {
+            if c != " "
+            {
+                newName += String(c)
+            }
+            
+        }
+        return newName + "," + number + " "
+    }
+    
+    
+    
+    @IBAction func sendString(_ sender: Any)
+    {
+        let password = generateString(name: (nameField.text)!, number: (numberField.text)!)
+        
+        for temp in password
+        {
+            simpleBluetoothIO.writeValue(value: Int8(temp.asciiValue! - UInt32(32)))
+        }
     }
 }
 
-extension ViewController: SimpleBluetoothIODelegate {
-    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: Int8) {
-        if value > 0 {
+extension ViewController: SimpleBluetoothIODelegate
+{
+    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: Int8)
+    {
+        if value > 0
+        {
             view.backgroundColor = UIColor.yellow
-        } else {
+        }
+        else
+        {
             view.backgroundColor = UIColor.black
         }
+    }
+}
+extension Character {
+    var asciiValue: UInt32? {
+        return String(self).unicodeScalars.filter{$0.isASCII}.first?.value
+    }
+}
+extension String {
+    var asciiAray: [UInt32] {
+            return unicodeScalars.filter{$0.isASCII}.map{$0.value}
     }
 }
